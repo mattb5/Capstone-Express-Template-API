8e71fef7493cd95da43375441865ba7674325349
mattb5 <mattbuffardi@gmail.com>
2016-07-29 16:35:07 -0400

    updating sign out curl request

--- scripts/sign-out.sh
+++ scripts/sign-out.sh
@@ -6,4 +6,4 @@ curl --include --request DELETE http://localhost:3000/sign-out/$ID \
 /////////////////////////////
 
 curl --include --request DELETE http://localhost:3000/sign-out/579b9e49fca688b6801f57d6 \
-  --header "Authorization: Token token=/B7ILoKW1pwnHtbCXPezCaIoVVxfE0JSpvtqnWPs+/0=--SN0KCc0hFNyCCa9DoRxIi/DWb8IyJskTAbNeaPCu2A8="
+  --header "Authorization: Token token=DHqF30sfTKKrPYHWmH+enQpb9GI00Pe8fKL5MFKX8dU=--A6sScVXdhrTX4Eh+JxWede3OfzeXouSfXCcud6/vIMQ="

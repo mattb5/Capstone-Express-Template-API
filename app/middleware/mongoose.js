8d03325dd8f726549e1c9af7e70d2bf869b08dde
mattb5 <mattbuffardi@gmail.com>
2016-08-02 11:47:35 -0400

    brian helping deploy to heroku

--- app/middleware/mongoose.js
+++ app/middleware/mongoose.js
@@ -1,15 +1,36 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const uri = process.env.MONGOLAB_URI || 'mongodb://localhost/matt-capstone-project';
-mongoose.Promise = global.Promise;
-mongoose.connect(uri);
-
-module.exports = mongoose;
+// const uri = process.env.MONGOLAB_URI || 'mongodb://localhost/matt-capstone-project';
 
 let uri;
 if (process.env.NODE_ENV === 'production') {
   uri = process.env.MONGODB_URI;
 } else {
-  uri = 'mongodb://localhost/ecommerce-express-api';
+  uri = 'mongodb://localhost/matt-capstone-project';
 }
+
+mongoose.Promise = global.Promise;
+mongoose.connect(uri);
+
+module.exports = mongoose;
+
+
+
+
+
+// 'use strict';
+//
+// const mongoose = require('mongoose');
+//
+// let uri;
+// if (process.env.NODE_ENV === 'production') {
+//   uri = process.env.MONGODB_URI;
+// } else {
+//   uri = 'mongodb://localhost/ecommerce-express-api';
+// }
+//
+// mongoose.Promise = global.Promise;
+// mongoose.connect(uri);
+//
+// module.exports = mongoose;
